fd56e1d63d0ea2c3950824bba0bfbc6cdb755c30
James Littlefield <jimilittle3@hotmail.com>
2020-07-21 18:12:05 -0400

    save dynamic font size test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,8 +4,34 @@
       id="top"
       :height="window.height"
       :width="window.width"
-      color="blue">
-      <h1>home</h1>
+      color="blue"
+      class="d-flex align-content-center flex-wrap">
+      <v-col cols="12">
+        <v-row class="ml-3 mr-3 pb-8 justify-center">
+          <!-- <v-card
+            v-for="n in 20"
+            :key="n"
+            class="pa-2"
+            outlined
+            tile
+          >
+            Flex item
+          </v-card> -->
+          <div :class="`text-${nameFontSize}`">{{window.width}}</div>
+        </v-row>
+
+        <v-row class="ml-3 mr-3 justify-center">
+          <v-card
+            v-for="m in 30"
+            :key="m"
+            class="pa-2"
+            outlined
+            tile
+          >
+            test
+          </v-card>
+        </v-row>
+      </v-col>
     </v-card>
     <v-card
       id="bottom">
@@ -28,6 +54,30 @@ export default class Home extends Vue {
   }
 
   // COMPUTED -------------------------
+  get nameFontSize() {
+    if (this.window.width <= 600) {
+      return "h1"
+    }
+
+    if (this.window.width >= 601 && this.window.width <= 960) {
+      return "caption"
+    }
+
+    if (this.window.width >= 961 && this.window.width <= 1264) {
+      return "h1"
+    }
+
+    if (this.window.width >= 1265 && this.window.width <= 1904) {
+      return "caption"
+    }
+
+    if (this.window.width >= 1905) {
+      return "h1"
+    }
+
+    return ""
+
+  }
 
   // EVENTS ---------------------------
 
