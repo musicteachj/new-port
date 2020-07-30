766fdf9e83e0f64e58c97884d3fc7c7ebc36a6dc
James Littlefield <jimilittle3@hotmail.com>
2020-07-29 22:55:43 -0400

    new format, shows

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,43 +1,68 @@
 <template>
   <div id="home">
     <v-card 
-      id="top"
-      :height="window.height"
+      id="topOne"
+      :height="window.height / 2"
       :width="window.width"
       color="blue"
-      class="d-flex align-content-center flex-wrap">
+      class="d-flex align-content-end flex-wrap">
       <v-col cols="12">
-        <v-row class="ml-3 mr-3 pb-8 justify-center">
+       <v-row class="ml-3 mr-3 mb-16 justify-center">
           <transition name="name">
-            <p v-if="show" :class="`text-${nameFontSize}`">{{window.width}}</p>
+            <p v-if="showName" :class="`text-${nameFontSize}`">{{window.width}}</p>
           </transition>
-        </v-row>
-
-        <v-row class="ml-3 mr-3 mt-4 text-center justify-center">
-          <div  
-            v-for="m in 30"
-            :key="m"
-            class="pa-2">
-            <!-- <transition :name="test(randId())"> -->
-               <transition name="nameNew">
-              <v-card width="50" height="50" v-if="condShow">
-                <p>test</p>
-              </v-card>
-              <!-- <p v-if="show">test</p> -->
-            </transition>
-          </div>
-          <!-- <v-card
-            v-for="m in 30"
-            :key="m"
-            class="pa-2"
-            outlined
-            tile
-          >
-            test
-          </v-card> -->
         </v-row>
       </v-col>
     </v-card>
+    <!-- <v-card 
+      id="topTwo"
+      :height="window.height / 2"
+      :width="window.width"
+      color="purple"
+      class="d-flex align-content-start flex-wrap"
+    >
+      <v-col cols="12">
+        <v-row class="ml-3 mr-3 justify-center" style="background-color:blue">
+          <div  
+            v-for="m in 5"
+            :key="m"
+            class="pa-2 mx-auto divIt"
+          >
+            <transition name="nameNew">    
+              <v-card v-once width="50" height="50" v-if="testShow(randId())">
+                <p>test</p>
+              </v-card>
+            </transition>
+          </div>
+        </v-row>
+      </v-col>
+    </v-card> -->
+
+    <v-card 
+      id="topTwo"
+      :height="window.height / 2"
+      :width="window.width"
+      color="purple"
+      class="d-flex align-content-start flex-wrap"
+    >
+      <v-col cols="12">
+        <v-row class="ml-3 mr-3 justify-center" style="background-color:blue">
+          <div  
+            v-for="s in shows"
+            :key="s.id"
+            class="pa-2 mx-auto divIt"
+          >
+            <transition name="nameNew">    
+              <v-card v-once width="50" height="50" v-if="s.show">
+                <p>test</p>
+              </v-card>
+            </transition>
+          </div>
+        </v-row>
+      </v-col>
+    </v-card>
+
+    
     <v-card
       id="bottom">
       <h1>more</h1>
@@ -57,9 +82,21 @@ export default class Home extends Vue {
     width: 0,
     height: 0
   };
-  show = false;
-  condShow = false;
-  numArr: Array<number> = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+  showName = false;
+  shows: any = [
+    {show: false, id: 1},
+    {show: false, id: 2},
+    {show: false, id: 3},
+    {show: false, id: 4},
+    {show: false, id: 5},
+  ]
+  // show1 = false;
+  // show2 = false;
+  // show3 = false;
+  // show4 = false;
+  // show5 = false;
+
+  numArr: Array<number> = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
 
 
   // COMPUTED -------------------------
@@ -99,13 +136,25 @@ export default class Home extends Vue {
   }
 
   mounted() {
-    setTimeout(() => {
-      this.condShow = true
-      setTimeout(() => {
-        this.show = true;
-      }, 2000)
-    }, 1000)
+    this.timeOutFunc();
+
+    // setTimeout(() => {
+    //   this.showName = true
+    // }, 1000);
+    // setTimeout(() => {
+    //   this.show1 = true
+    // }, 1100);
+    // setTimeout(() => {
+    //   this.show2 = true
+    // }, 1200)
+    // setTimeout(() => {
+    //   this.show = true
+    // }, 1000);
+    // setTimeout(() => {
+    //   this.show1 = true
+    // }, 1500)
     // this.show = true;
+    // this.show1 = true;
   }
 
   destroyed() {
@@ -113,20 +162,94 @@ export default class Home extends Vue {
   }
 
   // METHODS --------------------------
-  test(rec: number) {
-    console.log(rec);
-
-    if (rec % 2 == 0) {
-      console.log("this worked");
-      return "name";
-    } else {
-      return "nothing"
+  timeOutFunc() {
+    setTimeout(() => {
+      this.showName = true;
+      for (const s of this.shows) {
+      console.log(s);
+       setTimeout(() => {
+        s.show = true
+      }, 1000);
     }
+    }, 1000);
+    
+    // setTimeout(() => {
+    //   console.log(this.shows[0].show1);
+    //   this.shows[0].show1 = true
+    // }, 2000);
+    // setTimeout(() => {
+    //   this.shows[1].show2 = true
+    // }, 2500)
+    // setTimeout(() => {
+    //   this.shows[2].show3 = true
+    // }, 3000)
+    // setTimeout(() => {
+    //   this.shows[3].show4 = true
+    // }, 3500)
+    // setTimeout(() => {
+    //   this.shows[4].show5 = true
+    // }, 4000)
 
+    // setTimeout(() => {
+    //   this.showName = true
+    //   setTimeout(() => {
+    //     this.show1 = true
+    //      setTimeout(() => {
+    //       this.show2 = true
+    //        setTimeout(() => {
+    //         this.show3 = true
+    //          setTimeout(() => {
+    //           this.show4 = true
+    //           setTimeout(() => {
+    //             this.show5 = true
+    //           }, 4000)
+    //         }, 3500)
+    //       }, 3000)
+    //     }, 2500)
+    //   }, 2000);
+    // }, 1000);
   }
 
+  testShow(m: any) {
+    console.log("Fire testShow")
+    if (m === 1) return this.show1;
+    if (m === 2) return this.show2;
+    if (m === 3) return this.show3;
+    if (m === 4) return this.show4;
+    if (m === 5) return this.show5;
+  }
+
+  // t(item: any) {
+  //   if (item === 2) {
+  //     return "show"
+  //   } else {
+  //     return "show1"
+  //   }
+  // } 
+
+  // conditionalName(item: any) {
+  //   if (item === 1) {
+  //     return "name"
+  //   } else {
+  //     return "nameNew"
+  //   }
+  // }
+
+  // test(rec: number) {
+  //   console.log(rec);
+
+  //   if (rec % 2 == 0) {
+  //     console.log("this worked");
+  //     return "name";
+  //   } else {
+  //     return "nothing"
+  //   }
+
+  // }
+
   randId() {
-    return Math.floor((Math.random() * 30) + 1);
+    console.log("Fire randId")
+    return Math.floor((Math.random() * 5) + 1);
   }
   
   handleResize() {
@@ -150,10 +273,10 @@ export default class Home extends Vue {
 }
 
 .nameNew-enter-active {
-  transition: all 2s ease;
+  transition: all 1s ease;
 }
 .nameNew-leave-active {
-  transition: all 2s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+  transition: all 1s cubic-bezier(1.0, 0.5, 0.8, 1.0);
 }
 .nameNew-enter, .nameNew-leave-to {
   transform: translateY(100px);
@@ -163,5 +286,11 @@ export default class Home extends Vue {
 .card {
   background-color: white;
   border: 1px solid black;
+}
+
+.divIt {
+  background-color: green;
+  width: 75px;
+  height: 75px;
 }
 </style>
