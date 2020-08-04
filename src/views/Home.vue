9e5f965c30e4d53b1ae2c56707f0727ed2975654
James Littlefield <jimilittle3@hotmail.com>
2020-08-03 22:27:26 -0400

    randomize array, code cleanup, add data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,29 +14,6 @@
         </v-row>
       </v-col>
     </v-card>
-    <!-- <v-card 
-      id="topTwo"
-      :height="window.height / 2"
-      :width="window.width"
-      color="purple"
-      class="d-flex align-content-start flex-wrap"
-    >
-      <v-col cols="12">
-        <v-row class="ml-3 mr-3 justify-center" style="background-color:blue">
-          <div  
-            v-for="m in 5"
-            :key="m"
-            class="pa-2 mx-auto divIt"
-          >
-            <transition name="nameNew">    
-              <v-card v-once width="50" height="50" v-if="testShow(randId())">
-                <p>test</p>
-              </v-card>
-            </transition>
-          </div>
-        </v-row>
-      </v-col>
-    </v-card> -->
 
     <v-card 
       id="topTwo"
@@ -45,7 +22,7 @@
       color="purple"
       class="d-flex align-content-start flex-wrap"
     >
-      <v-col cols="12">
+      <v-col cols="12" class="mx-auto" style="background-color:red; max-width: 90% !important;">
         <v-row class="ml-3 mr-3 justify-center" style="background-color:blue">
           <div  
             v-for="s in shows"
@@ -54,7 +31,7 @@
           >
             <transition name="nameNew">    
               <v-card v-once width="50" height="50" v-if="s.show">
-                <p>test</p>
+                <p>{{s.tech}}</p>
               </v-card>
             </transition>
           </div>
@@ -74,7 +51,7 @@
 <script lang="ts">
 import Vue from 'vue';
 import Component from 'vue-class-component';
-import { set } from 'vue/types/umd';
+// import { set } from 'vue/types/umd';
 
 @Component({})
 export default class Home extends Vue {
@@ -85,20 +62,22 @@ export default class Home extends Vue {
   };
   showName = false;
   shows: any = [
-    {show: false, id: 1},
-    {show: false, id: 2},
-    {show: false, id: 3},
-    {show: false, id: 4},
-    {show: false, id: 5},
+    {show: false, id: 1, tech: "HTML"},
+    {show: false, id: 2, tech: "CSS"},
+    {show: false, id: 3, tech: "Sass"},
+    {show: false, id: 4, tech: "JavaScript"},
+    {show: false, id: 5, tech: "TypeScript"},
+    {show: false, id: 6, tech: "Express"},
+    {show: false, id: 7, tech: "GraphQL"},
+    {show: false, id: 8, tech: "MongoDB"},
+    {show: false, id: 9, tech: "Bootstrap"},
+    {show: false, id: 10, tech: "Vue"},
+    {show: false, id: 11, tech: "React"},
+    {show: false, id: 12, tech: "Vuex"},
+    {show: false, id: 13, tech: "Git"},
+    {show: false, id: 14, tech: "Node"},
+    {show: false, id: 15, tech: "BitBucket"},
   ]
-  // show1 = false;
-  // show2 = false;
-  // show3 = false;
-  // show4 = false;
-  // show5 = false;
-
-  numArr: Array<number> = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
-
 
   // COMPUTED -------------------------
   get nameFontSize() {
@@ -137,25 +116,7 @@ export default class Home extends Vue {
   }
 
   mounted() {
-    this.timeOutFunc();
-
-    // setTimeout(() => {
-    //   this.showName = true
-    // }, 1000);
-    // setTimeout(() => {
-    //   this.show1 = true
-    // }, 1100);
-    // setTimeout(() => {
-    //   this.show2 = true
-    // }, 1200)
-    // setTimeout(() => {
-    //   this.show = true
-    // }, 1000);
-    // setTimeout(() => {
-    //   this.show1 = true
-    // }, 1500)
-    // this.show = true;
-    // this.show1 = true;
+    this.newTimeOutFunc();
   }
 
   destroyed() {
@@ -163,123 +124,33 @@ export default class Home extends Vue {
   }
 
   // METHODS --------------------------
-  timeOutFunc() {
+  newTimeOutFunc() {
     let timer = 1500;
+    let idNumber = 1;
+
     setTimeout(() => {
-      this.showName = true;
-    //   for (const s of this.shows) {
-    //   console.log(s);
-    //    setTimeout(() => {
-    //     s.show = true
-    //   }, 1000);
-    // }
+      this.showName = true
     }, 1000);
-    // setTimeout(() => {
-    //   this.shows[0].show = true
-    // }, 1500);
-    //  setTimeout(() => {
-    //   this.shows[1].show = true
-    // }, 2000);
-    //  setTimeout(() => {
-    //   this.shows[2].show = true
-    // }, 2500);
-    //  setTimeout(() => {
-    //   this.shows[3].show = true
-    // }, 3000);
-    //  setTimeout(() => {
-    //   this.shows[4].show = true
-    // }, 3500)
-    
-    setTimeout(() => {
-      this.showName = true;
-      for (const s of this.shows) {
-        console.log(s);
-        timer = timer += 500;
-        setTimeout(() => {
-          s.show = true;
-        }, timer);
+
+    // Randomizes array
+    for (let i = this.shows.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [this.shows[i], this.shows[j]] = [this.shows[j], this.shows[i]];
+    }
+
+    for (let i = 0; i < this.shows.length; i++) {
+      for (const s of this.shows) { 
+        if (s.id === idNumber) {
+          timer = timer += 150;
+          idNumber = idNumber += 1;
+          setTimeout(() => {
+            s.show = true
+          }, timer);
+        }
       }
-    }, 1000)
-
-    // setTimeout(() => {
-    //   console.log(this.shows[0].show1);
-    //   this.shows[0].show1 = true
-    // }, 2000);
-    // setTimeout(() => {
-    //   this.shows[1].show2 = true
-    // }, 2500)
-    // setTimeout(() => {
-    //   this.shows[2].show3 = true
-    // }, 3000)
-    // setTimeout(() => {
-    //   this.shows[3].show4 = true
-    // }, 3500)
-    // setTimeout(() => {
-    //   this.shows[4].show5 = true
-    // }, 4000)
-
-    // setTimeout(() => {
-    //   this.showName = true
-    //   setTimeout(() => {
-    //     this.show1 = true
-    //      setTimeout(() => {
-    //       this.show2 = true
-    //        setTimeout(() => {
-    //         this.show3 = true
-    //          setTimeout(() => {
-    //           this.show4 = true
-    //           setTimeout(() => {
-    //             this.show5 = true
-    //           }, 4000)
-    //         }, 3500)
-    //       }, 3000)
-    //     }, 2500)
-    //   }, 2000);
-    // }, 1000);
+    }
   }
 
-  testShow(m: any) {
-    console.log("Fire testShow")
-    if (m === 1) return this.show1;
-    if (m === 2) return this.show2;
-    if (m === 3) return this.show3;
-    if (m === 4) return this.show4;
-    if (m === 5) return this.show5;
-  }
-
-  // t(item: any) {
-  //   if (item === 2) {
-  //     return "show"
-  //   } else {
-  //     return "show1"
-  //   }
-  // } 
-
-  // conditionalName(item: any) {
-  //   if (item === 1) {
-  //     return "name"
-  //   } else {
-  //     return "nameNew"
-  //   }
-  // }
-
-  // test(rec: number) {
-  //   console.log(rec);
-
-  //   if (rec % 2 == 0) {
-  //     console.log("this worked");
-  //     return "name";
-  //   } else {
-  //     return "nothing"
-  //   }
-
-  // }
-
-  randId() {
-    console.log("Fire randId")
-    return Math.floor((Math.random() * 5) + 1);
-  }
-  
   handleResize() {
     this.window.width = window.innerWidth;
     this.window.height = window.innerHeight;
@@ -307,7 +178,7 @@ export default class Home extends Vue {
   transition: all 1s cubic-bezier(1.0, 0.5, 0.8, 1.0);
 }
 .nameNew-enter, .nameNew-leave-to {
-  transform: translateY(100px);
+  transform: translateY(-100px);
   opacity: 0;
 }
 
