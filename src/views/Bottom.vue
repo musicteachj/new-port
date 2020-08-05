d4db27e57d8cea64f5d65fa022d71bb2ee31145c
James Littlefield <jimilittle3@hotmail.com>
2020-08-05 10:21:30 -0400

    rearrange structure of top and bottom, import assets for cards

---
+++ src/views/Bottom.vue
@@ -0,0 +1,96 @@
+<template>
+  <div id="bottomComp" style="background-color:green">
+    <v-row class="justify-center" style="background-color:blue">
+      <v-card v-for="(p, index) in projects" :key="index" class="mx-auto mt-8"
+            max-width="500">
+         <v-img
+          class="white--text"
+          height="400px"
+          :src="p.img">
+        </v-img>
+
+        <v-card-text>
+          <h1 class="text--primary display-1">{{p.name}}</h1>
+          <br>
+          <p class="text--primary body-2">
+            <span>{{p.text}}</span>
+          </p>
+        </v-card-text>
+
+        <v-chip
+          text-color="white"
+          :color="p.color"
+          v-for="t in p.tech"
+          :key="t"
+          class="ma-2">
+          {{t}}
+        </v-chip>
+
+        <v-card-actions>
+          <v-tooltip  :color="p.color" top v-if="index != 2 && index != 3">
+            <template v-slot:activator="{ on }">
+              <v-icon class="actionz" :color="p.color" dark v-on="on">mdi-account</v-icon>
+            </template>
+            <span>Individual</span>
+          </v-tooltip>
+          <v-tooltip :color="p.color" top v-if="index == 2 || index == 3">
+            <template v-slot:activator="{ on }">
+              <v-icon class="actionz" :color="p.color" dark v-on="on">mdi-account-multiple</v-icon>
+
+            </template>
+            <span>Collaboration</span>
+          </v-tooltip>
+          
+          <div class="flex-grow-1"></div>
+          <v-btn
+            
+            class="demo"
+            v-if="index > 1"
+            text
+            :color="p.color"
+            :href="p.code"
+            target="_blank">
+            Code
+          </v-btn>
+          <v-btn
+            class="code"
+            text
+            :color="p.color"
+            :href="p.demo"
+            target="_blank">
+            Demo
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-row>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+import CarlosImg from '@/assets/carlos.png';
+import GreatDebateImg from '@/assets/greatDebate.png';
+
+// const Carlos = require('@/assets/carlos.png');
+// const GreatDebate = require('@/assets/greatDebate.png');
+
+@Component({})
+export default class Bottom extends Vue {
+
+  projects: Array<object> = [
+    {name: "Carlos San Francisco", color: "#B30101", img: CarlosImg, text: "A web game inspired by Carmen Sandiego. The user must use clues given to them in the form of pictures taken around the world (Flickr API) and various facts given to them (CIA world fact book API) to try and track down the criminal on the world map before time runs out.", tech: ["HTML", "CSS", "JavaScript", "jQuery", "Bootstrap", "Firebase", "Flickr API"], demo: "https://carltheape.github.io/carlosSanFrancisco/", code: "https://github.com/carltheape/carlosSanFrancisco"},
+    {name: "The Great Debate", color: "#453C41", img: GreatDebateImg, text: "The Great DB8 is a full stack debate website inspired by Reddit that allows users to create an account and comment on admin curated topics.", tech: ["HTML", "CSS", "JavaScript", "jQuery", "Bootstrap", "Handlebars.js", "Node.js", "Express.js", "MySQL"], demo: "https://thegreatdb8.herokuapp.com/", code: "https://github.com/jliip51/The-Great-Debate"}
+    // {name: "Barcode Generator", color: "#453C41", img: "https://picsum.photos/id/11/500/300", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ", tech: ["HTML", "CSS", "JavaScript", "Node.js", "Express.js", "MongoDB", "Vue.js", "Vuetify", "Vue-Barcode", "PrintD"], demo: "https://github.com/musicteachj", code: "https://github.com/musicteachj"},
+    // {name: "Music Calendar", color: "#453C41", img: "https://picsum.photos/id/11/500/300", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ", tech: ["HTML", "CSS", "Sass", "JavaScript", "Node.js", "Express.js", "MongoDB", "Vue.js", "Vuetify"], demo: "https://github.com/musicteachj", code: "https://github.com/musicteachj"},
+    // {name: "React Project", color: "#453C41", img: "https://picsum.photos/id/11/500/300", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ", tech: ["HTML", "CSS", "JavaScript", "Node.js", "MySQL", "React.js"], demo: "https://github.com/musicteachj", code: "https://github.com/musicteachj"}
+
+  ];
+
+
+  
+
+}
+
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,48 +1,13 @@
 <template>
   <div id="home">
-    <v-card 
-      id="topOne"
-      :height="window.height / 2"
-      :width="window.width"
-      color="blue"
-      class="d-flex align-content-end flex-wrap">
-      <v-col cols="12">
-       <v-row class="ml-3 mr-3 mb-16 justify-center">
-          <transition name="name">
-            <p v-if="showName" :class="`text-${nameFontSize}`">{{window.width}}</p>
-          </transition>
-        </v-row>
-      </v-col>
+     <v-card
+      id="top">
+      <Top />
     </v-card>
 
-    <v-card 
-      id="topTwo"
-      :height="window.height / 2"
-      :width="window.width"
-      color="purple"
-      class="d-flex align-content-start flex-wrap"
-    >
-      <v-col cols="12" class="mx-auto" style="background-color:red; max-width: 90% !important;">
-        <v-row class="ml-3 mr-3 justify-center" style="background-color:blue">
-          <div  
-            v-for="s in shows"
-            :key="s.id"
-            class="pa-2 mx-auto divIt"
-          >
-            <transition name="nameNew">    
-              <v-card v-once width="50" height="50" v-if="s.show">
-                <p>{{s.tech}}</p>
-              </v-card>
-            </transition>
-          </div>
-        </v-row>
-      </v-col>
-    </v-card>
-
-    
     <v-card
       id="bottom">
-      <h1>more</h1>
+      <Bottom />
     </v-card>
     
   </div>
@@ -51,148 +16,21 @@
 <script lang="ts">
 import Vue from 'vue';
 import Component from 'vue-class-component';
+import Top from '@/views/Top.vue';
+import Bottom from '@/views/Bottom.vue';
 // import { set } from 'vue/types/umd';
 
-@Component({})
+@Component({
+  components: {
+    Top,
+    Bottom
+  }
+})
 export default class Home extends Vue {
-  // LOCAL VARIABLES ------------------
-   window: any = {
-    width: 0,
-    height: 0
-  };
-  showName = false;
-  shows: any = [
-    {show: false, id: 1, tech: "HTML"},
-    {show: false, id: 2, tech: "CSS"},
-    {show: false, id: 3, tech: "Sass"},
-    {show: false, id: 4, tech: "JavaScript"},
-    {show: false, id: 5, tech: "TypeScript"},
-    {show: false, id: 6, tech: "Express"},
-    {show: false, id: 7, tech: "GraphQL"},
-    {show: false, id: 8, tech: "MongoDB"},
-    {show: false, id: 9, tech: "Bootstrap"},
-    {show: false, id: 10, tech: "Vue"},
-    {show: false, id: 11, tech: "React"},
-    {show: false, id: 12, tech: "Vuex"},
-    {show: false, id: 13, tech: "Git"},
-    {show: false, id: 14, tech: "Node"},
-    {show: false, id: 15, tech: "BitBucket"},
-    {show: false, id: 16, tech: "Jira"},
-    {show: false, id: 17, tech: "Azure"},
-    {show: false, id: 18, tech: "Vuetify"},
-  ]
-
-  // COMPUTED -------------------------
-  get nameFontSize() {
-    // if (this.window.width <= 600) {
-    //   return "h1"
-    // }
-
-    // if (this.window.width >= 601 && this.window.width <= 960) {
-    //   return "caption"
-    // }
-
-    // if (this.window.width >= 961 && this.window.width <= 1264) {
-    //   return "h1"
-    // }
-
-    // if (this.window.width >= 1265 && this.window.width <= 1904) {
-    //   return "caption"
-    // }
-
-    // if (this.window.width >= 1905) {
-    //   return "h1"
-    // }
-
-    // return ""
-
-    return "h1"
-
-  }
-
-  // EVENTS ---------------------------
-
-  // LIFECYCLE EVENTS -----------------
-  created() {
-    window.addEventListener('resize', this.handleResize)
-    this.handleResize();
-  }
-
-  mounted() {
-    this.newTimeOutFunc();
-  }
-
-  destroyed() {
-    window.removeEventListener('resize', this.handleResize)
-  }
-
-  // METHODS --------------------------
-  newTimeOutFunc() {
-    let timer = 1500;
-    let idNumber = 1;
-
-    setTimeout(() => {
-      this.showName = true
-    }, 1000);
-
-    // Randomizes array
-    for (let i = this.shows.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [this.shows[i], this.shows[j]] = [this.shows[j], this.shows[i]];
-    }
-
-    for (let i = 0; i < this.shows.length; i++) {
-      for (const s of this.shows) { 
-        if (s.id === idNumber) {
-          timer = timer += 150;
-          idNumber = idNumber += 1;
-          setTimeout(() => {
-            s.show = true
-          }, timer);
-        }
-      }
-    }
-  }
-
-  handleResize() {
-    this.window.width = window.innerWidth;
-    this.window.height = window.innerHeight;
-  }
 
 }
 </script>
 
 <style scoped>
-.name-enter-active {
-  transition: all 2s ease;
-}
-.name-leave-active {
-  transition: all 2s cubic-bezier(1.0, 0.5, 0.8, 1.0);
-}
-.name-enter, .name-leave-to {
-  transform: translateY(-100px);
-  opacity: 0;
-}
 
-.nameNew-enter-active {
-  transition: all 1s ease;
-}
-.nameNew-leave-active {
-  transition: all 1s cubic-bezier(1.0, 0.5, 0.8, 1.0);
-}
-.nameNew-enter, .nameNew-leave-to {
-  transform: translateY(-100px);
-  opacity: 0;
-}
-
-.card {
-  background-color: white;
-  border: 1px solid black;
-}
-
-.divIt {
-  background-color: green;
-  width: 75px;
-  height: 75px;
-}
 </style>
