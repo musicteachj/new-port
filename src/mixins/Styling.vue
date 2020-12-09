cc78ed8f4ac330ce192109eae16ecff665281587
James Littlefield <jimilittle3@hotmail.com>
2020-12-09 09:35:43 -0500

    convert Bottom.vue to mixin for styling

--- src/mixins/Styling.vue
+++ src/mixins/Styling.vue
@@ -1,42 +1,32 @@
 <script lang="ts">
 import Vue from 'vue';
 import Component from 'vue-class-component';
-// import { mixin } from 'vue/types/umd';
-// import Top from '@/views/Top.vue';
-// import Bottom from '@/views/Bottom.vue';
-// import { set } from 'vue/types/umd';
 
 @Component
 export default class Styling extends Vue {
 
   // COMPUTED -------------------------
   get nameFontSize() {
-    // if (this.window.width <= 600) {
-    //   return "h1"
-    // }
-
-    // if (this.window.width >= 601 && this.window.width <= 960) {
-    //   return "caption"
-    // }
-
-    // if (this.window.width >= 961 && this.window.width <= 1264) {
-    //   return "h1"
-    // }
-
-    // if (this.window.width >= 1265 && this.window.width <= 1904) {
-    //   return "caption"
-    // }
-
-    // if (this.window.width >= 1905) {
-    //   return "h1"
-    // }
-
-    // return ""
-
-    return "h2"
-
+    switch (this.$vuetify.breakpoint.name) {
+      case 'xs': return 'h4';
+      case 'sm': return 'h3';
+      case 'md': return 'h2';
+      case 'lg': return 'h1';
+      case 'xl': return 'h1';
+      default: return 'h1';
+    }
   }
 
+  get projectsText() {
+    switch (this.$vuetify.breakpoint.name) {
+      case 'xs': return 'h4';
+      case 'sm': return 'h3';
+      case 'md': return 'h2';
+      case 'lg': return 'h1';
+      case 'xl': return 'h1';
+      default: return 'h1';
+    }
+  }
 }
 </script>
 
